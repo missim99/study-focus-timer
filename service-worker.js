bd595ad975cfdc177face7a31ed6f010ff57b920
missim99 <51298060+missim99@users.noreply.github.com>
2026-09-23 20:59:11 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,93 @@
+const CACHE_NAME = "study-focus-timer-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png",
+  "./ambient-calm.mp3",
+  "./applause.mp3"
+];
+
+
+/* INSTALL */
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(
+          FILES_TO_CACHE
+        );
+
+      })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+/* ACTIVATE */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches
+      .keys()
+      .then(cacheNames => {
+
+        return Promise.all(
+
+          cacheNames.map(name => {
+
+            if (name !== CACHE_NAME) {
+
+              return caches.delete(name);
+
+            }
+
+          })
+
+        );
+
+      })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+/* FETCH */
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+
+    caches
+      .match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
+});
